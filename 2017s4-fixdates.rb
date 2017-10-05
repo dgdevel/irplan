737e8bcd721c7e2a977d235dddba0125576e871d
milky <milky@archmini>
2017-10-05 19:21:48 +0000

    fix 2017s4 october dates

---
+++ 2017s4-fixdates.rb
@@ -0,0 +1,12 @@
+# Encoding: utf-8
+
+require 'date'
+
+require './model'
+
+Weeks.update(10, :start => Date.new(2017,10,03))
+Weeks.update(11, :start => Date.new(2017,10,10))
+Weeks.update(12, :start => Date.new(2017,10,17))
+Weeks.update(13, :start => Date.new(2017,10,24))
+Weeks.update(14, :start => Date.new(2017,10,31))
+
