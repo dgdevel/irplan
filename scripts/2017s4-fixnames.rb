b104b1adcf2e153c98ab4e1f597bf88b5dd737c7
milky <milky@archmini>
2017-10-06 09:13:30 +0000

    fix track names

---
+++ scripts/2017s4-fixnames.rb
@@ -0,0 +1,9 @@
+# Encoding: utf-8
+
+require 'date'
+
+require './model'
+
+Weeks.update(9, :track_name => 'Autodromo Internazionale Enzo e Dino Ferrari (28 laps)')
+Weeks.update(10, :track_name => 'Snetterton - 200 Circuit (40 laps)')
+
