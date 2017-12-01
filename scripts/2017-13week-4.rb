9312d73e8b93dc075ce5b32f4f1d6a8ee68e16bb
milky <milky@archmini>
2017-12-01 22:46:44 +0100

    w13 script definitive names

--- scripts/2017-13week-4.rb
+++ scripts/2017-13week-4.rb
@@ -3,47 +3,47 @@ require 'date'
 require './model'
 
 w13event = Events.create(
-  :name => 'Lotus 79 - 13th week event'
+  :name => 'Classic Team Championship 2018 S1 Season Preview'
 )
 
 EventRaces.create(
   :events_id => w13event.id,
-  :track_name => 'track 1',
+  :track_name => 'Indianapolis Motor Speedway, Road Course, 15 laps',
   :index => 1,
-  :start => DateTime.new(2017,12,5,20,30)
+  :start => DateTime.new(2017,12,4,20,30)
 )
 
 EventRaces.create(
   :events_id => w13event.id,
-  :track_name => 'track 2',
+  :track_name => 'Watkins Glen, Classic Boot, 14 laps',
   :index => 2,
   :start => DateTime.new(2017,12,6,20,30)
 )
 
 EventRaces.create(
   :events_id => w13event.id,
-  :track_name => 'track 3',
+  :track_name => 'Nürburgring Nordschleife, Industriefahrten, 4 laps (late afternoon skybox)',
   :index => 3,
   :start => DateTime.new(2017,12,7,20,30)
 )
 
 EventRaces.create(
   :events_id => w13event.id,
-  :track_name => 'track 4',
+  :track_name => 'Suzuka International Racing Course, Grand Prix, 12 laps',
   :index => 4,
   :start => DateTime.new(2017,12,8,20,30)
 )
 
 EventRaces.create(
   :events_id => w13event.id,
-  :track_name => 'track 5',
+  :track_name => 'Mount Panorama Circuit, 11 laps',
   :index => 5,
   :start => DateTime.new(2017,12,9,14,30)
 )
 
 EventRaces.create(
   :events_id => w13event.id,
-  :track_name => 'track 6',
+  :track_name => 'Road Atlanta, Full Course, 18 laps',
   :index => 6,
   :start => DateTime.new(2017,12,10,16,30)
 )
